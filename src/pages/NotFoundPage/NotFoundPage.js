e82ae5e2addcc5dc315abb17572a1681c99fd794
Tsveta Koleva <tsveta.koleva@sendinblue.com>
2022-12-19 10:44:37 +0200

    Add exclamation mark

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => (
         <Header />
         <Container className="not-found-page__container">
             <h1>404</h1>
-            <h2>Page Not Found</h2>
+            <h2>Page Not Found!</h2>
         </Container>
     </div>
     
